d3aa36d578293093ce9cd8bf32d924fa167854d1
Sumitpal0572 <asmyselfsumit@gmail.com>
2025-06-09 22:28:47 +0530

    find duplicate using bruteforce

--- Array/ContainsDuplicate.js
+++ Array/ContainsDuplicate.js
@@ -47,3 +47,17 @@ function containsDuplicateUsingSort(nums) {
 }
 
 console.log(containsDuplicateUsingSort(nums))
+
+
+function containsDuplicateUsingBruteforce(nums) {
+    for (let i = 0; i < nums.length - 1; i++) {
+        for (let j = i + 1; j < nums.length; j++) {
+            if (nums[i] === nums[j]) {
+                return true; // Duplicate found
+            }
+        }
+    }
+    return false; // No duplicates
+}
+
+console.log(containsDuplicateUsingBruteforce(nums));
