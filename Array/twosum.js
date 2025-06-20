726b214040642f7c9c6e1530b7faa6a80b70de08
Sumitpal0572 <asmyselfsumit@gmail.com>
2025-06-20 21:58:33 +0530

    find twosum

--- Array/twosum.js
+++ Array/twosum.js
@@ -1,15 +1,15 @@
-function findTwoSumIndices(nums, target){
-    let left = 0, right = nums.length-1;
+function findTwoSumIndices(nums, target) {
+    let left = 0, right = nums.length - 1;
 
-    while(left < right) {
+    while (left < right) {
         const total = nums[left] + nums[right];
 
-        if(total === target) {
-            return [left+1, right+1]; // Return 1-based indices 
-        } else if(total > target) {
-            right--; //Move the right pointer leftward
+        if (total === target) {
+            return [left + 1, right + 1]; 
+        } else if (total > target) {
+            right--; 
         } else {
-            left++; //Move the left pointer rightward
+            left++; 
         }
     }
 
@@ -17,7 +17,7 @@ function findTwoSumIndices(nums, target){
 }
 
 
-let nums1 = [2,7,11,15,9];
+let nums1 = [2, 7, 11, 15, 9];
 let target1 = 16;
 
-console.log(findTwoSumIndices(nums1,target1))
+console.log(findTwoSumIndices(nums1, target1))
