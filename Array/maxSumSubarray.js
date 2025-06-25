75d94946aee65fd5d9c9238b7f800624603187fc
Sumitpal0572 <asmyselfsumit@gmail.com>
2025-06-25 23:35:11 +0530

    solved

--- Array/maxSumSubarray.js
+++ Array/maxSumSubarray.js
@@ -30,3 +30,19 @@
 // This algorithm has a time complexity of O(n), where n is the number of elements. This is because we iterate the array at most once.
 
 // Here, we don't use any additional datastructure other than the two sum variables. Hence, the space complexity will be O(1).
+
+function maxSumSubarray(nums) {
+    if (nums.length === 0) return 0;
+    let currentMaxSum = globalMaxSum = nums[0];
+    for (let i = 1; i < nums.length; i++) {
+        currentMaxSum = Math.max(nums[i], currentMaxSum + nums[i]);
+        globalMaxSum = Math.max(currentMaxSum, globalMaxSum);
+    }
+    return globalMaxSum;
+}
+
+// Test cases
+const numbers = [-7, -2, 3, 4, -5, 6, 7, -2];
+const emptyNumbers = [];
+console.log("Max Subarray Sum (numbers):", maxSumSubarray(numbers));
+console.log("Max Subarray Sum (emptyNumbers):", maxSumSubarray(emptyNumbers));
