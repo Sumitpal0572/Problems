d7ba064f96913d7bb0bedf5ce950ffcf18959a25
Sumitpal0572 <asmyselfsumit@gmail.com>
2025-07-13 18:28:15 +0530

    using sorting find largest element

--- Array/LargestElement.js
+++ Array/LargestElement.js
@@ -14,7 +14,29 @@ function largestElement(arr) {
 }
 
 array = [5, 8, 3, 2, 4];
-console.log("lagest element:", largestElement(array))
+console.log("largest element:", largestElement(array))
 
-// Optimal approach is this : let first create a variable and assume the index 0 will be the largest element and then compare the others element and then update the largest element and update it and retrun it 
-// Time complexity = 0(N)
+// Optimal approach is this : let first create a variable and assume the index 0 will be the largest element and then compare the others element and then update the largest element and update it and retrun it
+// Time complexity = O(N) , space complexity=O(1)
+
+
+// find the largest element using Sorting 
+
+function largestElementUsingSort(arr) {
+    if (arr.length === 0) {
+        return null;
+    }
+
+    arr.sort(function (a, b) {
+        return a - b;
+    });
+
+    return (arr[arr.length - 1])
+}
+
+let array = [5, 9, 6, 8, 7]
+console.log("Largest Element:", largestElementUsingSort(array));
+
+
+//  find the largest element using sorting it will arrange the element in ascending order then return it .
+// time complexity = O(N log N) Space complexity =O(1)
