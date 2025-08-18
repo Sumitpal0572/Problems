bc18ebb0d6b492445e074dc2939e375e5cf2b0e0
Sumitpal0572 <asmyselfsumit@gmail.com>
2025-08-18 22:30:15 +0530

    INR qustn done

---
+++ Basic/INR_notes.js
@@ -0,0 +1,33 @@
+let amount = 7562;
+if (amount >= 500) {
+    console.log(Math.floor(amount / 500));
+    amount = amount % 500
+}
+if (amount >= 200) {
+    console.log(Math.floor(amount / 200));
+    amount = amount % 200
+}
+if (amount >= 100) {
+    console.log(Math.floor(amount / 100));
+    amount = amount % 100
+}
+if (amount >= 50) {
+    console.log(Math.floor(amount / 50));
+    amount = amount % 50
+}
+if (amount >= 20) {
+    console.log(Math.floor(amount / 20));
+    amount = amount % 20
+}
+if (amount >=10 ) {
+    console.log(Math.floor(amount / 10));
+    amount = amount % 10
+}
+if (amount >= 5) {
+    console.log(Math.floor(amount / 5));
+    amount = amount % 5
+}
+if (amount >= 2) {
+    console.log(Math.floor(amount / 2));
+    amount = amount % 2
+}
